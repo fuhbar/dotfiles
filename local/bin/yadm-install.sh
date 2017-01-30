e18fbc267a3e2098297310a3a14d28031bd4cd4a
Fabian Dörk <fabian.doerk@de.clara.net>
2017-01-31 00:01:16 +0100

    adds more to be installable items

--- local/bin/yadm-install.sh
+++ local/bin/yadm-install.sh
@@ -3,4 +3,9 @@
 # This is just a list of commands need to be ran in order to complete the
 # dotfiles setup and related components. 
 
+cd ~/local/etc
+brew bundle install
+
+pip -r requirements.txt
+
 $HOME/local/src/fzf/install
